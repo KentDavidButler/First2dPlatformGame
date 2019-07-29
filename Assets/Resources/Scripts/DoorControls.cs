0bddb40c15c7c3688850ef010b350132eedaca08
Kent Butler-HomeDesktop <KentDavidButler@gmail.com>
2019-07-29 19:32:59 -0400

    Adding in Doors and switches

---
+++ Assets/Resources/Scripts/DoorControls.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoorControls : MonoBehaviour
+{
+
+    public Animator animatorDoorTop;
+    public Animator animatorDoorBtm;
+    private bool isOpen;
+
+    public GameObject DoorSwitch;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        animatorDoorTop.SetBool("IsOpen", isOpen);
+        animatorDoorBtm.SetBool("IsOpen", isOpen);
+    }
+}
