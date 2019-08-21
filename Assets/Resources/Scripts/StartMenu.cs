f5e547b45788962be600915a5c14d3b1fc4068a6
Kent Butler-HomeDesktop <KentDavidButler@gmail.com>
2019-08-20 21:32:00 -0400

    Starting to add Main menu

---
+++ Assets/Resources/Scripts/StartMenu.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class StartMenu : MonoBehaviour
+{
+
+    public void StartButton()
+    {
+        SceneManager.LoadScene("LvL-One");
+        Debug.Log("Going-To-Level-One");
+    }
+
+    public void MenuButton()
+    {
+        Debug.Log("Going-To-The-Menus");
+    }
+
+    public void ExitButton()
+    {
+        Application.Quit();
+        Debug.Log("Exiting Game");
+    }
+}
